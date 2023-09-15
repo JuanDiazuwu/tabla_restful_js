fbc3d2dd3b180d1e83375b6c9345e8322628c68f
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2023-09-15 16:08:48 -0600

    Subiendo index.js, package.json y .gitignore

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+//const resultsRouter = require('./routers/results')
+
+//app.use('/results', resultsRouter);
+
+app.post('/results/:x/:y', (req, res) =>{
+    const {x, y} = req.params;
+    res.send((Number(x) * Number(y)).toString());
+});
+
+app.get('/results/:x/:y', (req, res) => {
+    const {x, y} = req.params;
+    res.send((Number(x) + Number(y)).toString());
+});
+
+app.get('/results/', (req, res) =>{
+    res.send('Results list');
+});
+
+app.put('/results/:x/:y', (req, res) => {
+    const {x, y} = req.params;
+    res.send((Number(x) / Number(y)).toString());
+});
+
+app.patch('/results/:x/:y', (req, res) => {
+    const {x, y} = req.params;
+    res.send((Number(x) ** Number(y)).toString());
+});
+
+app.delete('/results/:x/:y', (req, res) => {
+    const {x, y} = req.params;
+    res.send((Number(x) - Number(y)).toString());
+});
+
+app.listen(6969);
